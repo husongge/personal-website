aad0f6b4c1d0b6f7588281712aa2b67533f895fa
husongge <husongge@tal.com>
2025-07-28 18:15:42 +0800

    Update project name to personal-website

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/me/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/personal-website/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
